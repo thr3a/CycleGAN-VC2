53acdb83f76ea32559592b1adf9a3177df5fbcf7
tahara <tahara@userlocal.jp>
2021-05-13 13:26:34 +0900

    add:

--- preprocess_training.py
+++ preprocess_training.py
@@ -86,8 +86,8 @@ def preprocess_for_training(train_A_dir, train_B_dir, cache_folder):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Prepare data for training Cycle GAN using PyTorch')
-    train_A_dir_default = './data/S0913/'
-    train_B_dir_default = './data/gaoxiaosong/'
+    train_A_dir_default = './data/inputA/'
+    train_B_dir_default = './data/inputB/'
     cache_folder_default = './cache/'
 
     parser.add_argument('--train_A_dir', type=str,
